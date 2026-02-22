5d00d67eba9961ab3c3a56f891ee34f3ac97e20c
rochaedgar-max <rocha.edgar@uabc.edu.mx>
2026-02-21 22:51:03 -0800

    Update2 BibliotecaPersonal.py
    Ya agregué las dos primeras funciones, pero voy a modificarlas para adaptarlas a la clase que crearon en las ramas anteriores.

--- BibliotecaPersonal.py
+++ BibliotecaPersonal.py
@@ -1,1 +1,31 @@
-a
+#Crear un sistema para insertar: al inicio, al final, o en orden alfabético por título.
+#Consultar: recorrido normal e inverso, búsquedas específicas
+#Actualizar: modificar datos de un libro existente.
+#Eliminar: remover un libro y mantener los enlaces correctos.
+# Nota: Debo manejar casos especiales (lista vacia, un solo elemento, eliminación de cabeza/cola)
+""" Primer intento: prototipo de las funciones. """
+
+class BibliotecaPersonal:
+  def __init__(self):
+    self.cabeza = None
+    self.cola = None
+
+  def insertar_libro_al_inicio(self, libro):
+    nuevo_libro = NodoLibro(libro)
+
+    if self.cabeza is None:
+      self.cabeza = self.cola = nuevo_objeto
+    else:
+     nuevo_libro.siguiente = self.cabeza
+      self.cabeza.anterior = nuevo_libro
+      self.cabeza = nuevo_libro
+
+  def insertar_libro_al_final(self, libro):
+    nuevo_libro = NodoLibro(libro)
+
+    if self.cabeza is None:
+      self.cabeza = self.cola = nuevo_objeto
+    else:
+      nuevo_objeto.anterior = self.cola
+      self.cola.siguiente = nuevo_objeto
+      self.cola = nuevo_objeto
